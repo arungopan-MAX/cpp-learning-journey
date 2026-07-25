f8979e7a05a0c1a8bcce88b0fdf69a8dc1524236
arungopan-MAX <arungopan012@gmail.com>
2026-07-25 19:23:31 +0530

    Implement sphere volume calculation
    Fixed syntax errors and calculations for sphere volume.

---
+++ projects/cube_of_sphere.cpp
@@ -0,0 +1,11 @@
+#include<studio>
+using namespace std;
+int main()
+{
+  float cube,radius;
+  cout<<"enter radius of sphere"<<cubee;
+  cin>>radius;
+  area=4/3*3.142*radius*radius;
+  cout<<"cube of sphere"<<cube;
+  return 0;
+}
