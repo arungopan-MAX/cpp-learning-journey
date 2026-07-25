d047a42769b890a1b055dcc73bc35bb1f653ba6a
arungopan-MAX <arungopan012@gmail.com>
2026-07-25 12:01:58 +0530

    Add main.cpp to demonstrate basic data types

---
+++ 01-Basics/Data types/main.cpp
@@ -0,0 +1,18 @@
+ #include <iostream>
+#include <string>
+
+int main() {
+    int age = 18;
+    float height = 168.5;
+    char grade = 'A';
+    bool isStudent = true;
+    string name = "Arun";
+
+    cout << "Name: " << name << std::endl;
+    cout << "Age: " << age << std::endl;
+    cout << "Height: " << height << std::endl;
+    cout << "Grade: " << grade << std::endl;
+    cout << "Student: " << isStudent << std::endl;
+
+    return 0;
+}
