0612ae06e029b1f03543479d4f1731e8c7bf420f
arungopan-MAX <arungopan012@gmail.com>
2026-07-25 19:38:48 +0530

    Add if statement for voting eligibility check

---
+++ 01-Basics/IF_function/if_statements.cpp
@@ -0,0 +1,17 @@
+#include<studio>
+using namespace std;
+int main()
+{
+  int a;
+  cout<<"enter your age";
+  cin>>a;
+  if(a>18)
+  {
+    cout<<"you are eligile for voting";
+  }
+  else
+  {
+    cout<<"you are not eligile for voting";
+  }
+  return 0;
+}
