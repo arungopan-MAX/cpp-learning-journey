b74d96119d92320e22c25695a8d2ffc4b714992c
arungopan-MAX <arungopan012@gmail.com>
2026-07-25 18:36:25 +0530

    Add basic calculator functionality in calculator.cpp

---
+++ 01-Basics/pojects/calculator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+      int a,b,c,d,e,f,g,h;
+      a=90;
+      b=123;
+      c=a+b;
+  cout<<"addition= "<<c;
+      d=a-b;
+  cout<<"subtraction= "<<d;
+      e=a/b;
+  cout<<"division= "<<e;
+      f=a*b;
+  cout<<"multiplication= "<<f;
+      g=a%b;
+  cout<<"modulus="<<g;
+    return 0;
+}
