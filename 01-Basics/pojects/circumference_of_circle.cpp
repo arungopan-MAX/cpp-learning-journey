2a38d30290301441bf62fdad67273108aab5f19f
arungopan-MAX <arungopan012@gmail.com>
2026-07-25 19:14:28 +0530

    Fix input prompt for radius in circumference calculation

--- 01-Basics/pojects/circumference_of_circle.cpp
+++ 01-Basics/pojects/circumference_of_circle.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
   float area,radius;
-  cout<<"enter circumference of circle"<<area;
+  cout<<"enter radius of circle"<<area;
   cin>>radius;
   area=2*3.142*radius;
   cout<<"circumference of circle"<<area;
