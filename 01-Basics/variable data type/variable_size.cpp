569996b5fb2825b1a0540cc4f79acc39e3a28a49
arungopan-MAX <arungopan012@gmail.com>
2026-07-25 18:02:55 +0530

    Add variable_size.cpp to display data type sizes

---
+++ 01-Basics/variable data type/variable_size.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int a;
+    float b;
+    double c;
+    char d;
+  cout<<"size of int= \n"<<sizeof(a);
+  cout<<"size of float= \n"<<sizeof(b);
+  cout<<"size of double= \n"<<sizeof(c);
+  cout<<"size of char= \n"<<sizeof(d);
+    return 0;
+}
