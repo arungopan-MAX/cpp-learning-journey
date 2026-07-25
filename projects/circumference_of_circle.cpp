9ba41a532de5d8d052faf2c9d76032e7ebae718e
arungopan-MAX <arungopan012@gmail.com>
2026-07-25 19:12:13 +0530

    Add program to calculate circumference of a circle

---
+++ projects/circumference_of_circle.cpp
@@ -0,0 +1,11 @@
+#include<studio>
+using namespace std;
+int main()
+{
+  float area,radius;
+  cout<<"enter circumference of circle"<<area;
+  cin>>radius;
+  area=2*3.142*radius;
+  cout<<"circumference of circle"<<area;
+  return 0;
+}
