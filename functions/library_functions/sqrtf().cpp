dbb4679b8a707ad7275365b4dc6a83ad65ef5efd
arungopan-MAX <arungopan012@gmail.com>
2026-07-26 21:23:41 +0530

    Implement sqrtf function example in new cpp file

---
+++ functions/library_functions/sqrtf().cpp
@@ -0,0 +1,15 @@
+#include<cmath>
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+  float val1=225.0;
+  float val2=300.0;
+  cout<<fixed<<setprecision(12)<<sqrtf(val1)<<endl;
+  cout<<fixed<<setprecision(12)<<sqrtf(val1)<<endl;
+  
+  return 0;
+} 
+}
