e3ca2e458339c68dbc3d3603d02d93f17e28bf5d
arungopan-MAX <arungopan012@gmail.com>
2026-07-26 21:12:42 +0530

    Implement square root calculation in sqrt().cpp
    Fix variable name typo and include necessary headers.

---
+++ functions/library_functions/sqrt().cpp
@@ -0,0 +1,14 @@
+#include<cmath>
+#include<inmanip>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  double val1=225.0;
+  double val2=300.0;
+  cout<<fixed<<setprecision(12)<<sqrt(vol1);
+  cout<<fixed<<setprecision(12)<<sqrt(vol2);
+
+  return 0;
+}
