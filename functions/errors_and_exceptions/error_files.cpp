e04214c9cb54ed8e41532751d13b166b1cd8608f
arungopan-MAX <arungopan012@gmail.com>
2026-07-26 21:27:22 +0530

    Implement square root calculation with error handling
    Fix variable declaration and output formatting for square root calculation.

---
+++ functions/errors_and_exceptions/error_files.cpp
@@ -0,0 +1,12 @@
+ #include<cmath>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  double a=-2,answer;
+  double = sqrt(a);
+  cout<<"square root of"<<a<<"is"<<answer<<endl;
+
+  return 0;
+}
