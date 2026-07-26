8d35c7286242038e6eb906920136e09f6db7f46a
arungopan-MAX <arungopan012@gmail.com>
2026-07-26 21:30:24 +0530

    Correct square root calculation in error_files.cpp
    Fix the calculation of the square root and variable initialization.

--- functions/errors_and_exceptions/error_files.cpp
+++ functions/errors_and_exceptions/error_files.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main()
 {
-  double a=-2,answer;
-  double = sqrt(a);
+  double answer;
+  double = sqrt();
   cout<<"square root of"<<a<<"is"<<answer<<endl;
 
   return 0;
