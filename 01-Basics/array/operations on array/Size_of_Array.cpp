414fed16e8b66ccb247ad9d6ca2dde54b3441799
arungopan-MAX <arungopan012@gmail.com>
2026-07-27 19:21:05 +0530

    Add Size_of_Array.cpp to demonstrate array size

---
+++ 01-Basics/array/operations on array/Size_of_Array.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char arr[] = {'a', 'b', 'c', 'd', 'f'};
+
+    
+    cout << "Size of arr[0]: " << sizeof(arr[0])
+    << endl;
+
+    
+    cout << "Size of arr: " << sizeof(arr) << endl;
+
+    
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Length of an array: " << n << endl;
+
+    return 0;
+}
